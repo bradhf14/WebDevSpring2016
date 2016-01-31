90c30ee9d8c74be4e543da431146cbf20063b011
BRADLEY HOWE <howe.b@husky.neu.edu>
2016-01-30 23:27:59 -0500

    Continued working on assignment, most base pages set up and server.js added.

---
+++ public/assignment/server.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Bradley on 1/30/16.
+ */
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/public'));
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+app.listen(port, ipaddress);

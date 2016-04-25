14c31c8fd8fb229262a8240d2461c66588b02685
BRADLEY HOWE <howe.b@husky.neu.edu>
2016-04-25 15:33:51 -0400

    Updated server file

--- server.js
+++ server.js
@@ -9,13 +9,16 @@ var session       = require('express-session');
 //goes into node modules and directory and look for mongoose library
 var mongoose      = require('mongoose');
 
+
 //will likely need to change the DB name
-mongoose.connect('mongodb://localhost/NewDB');
 //console.log(mongoose);
 
 
 // connect to the database
-var db = "Figure this out later";//mongoose.connect(connectionString);
+var db = mongoose.connect('mongodb://localhost/NewDB');
+
+
+//"Figure this out later";//mongoose.connect(connectionString);
 
 
 //TODO figure out how to attach schemas
@@ -24,8 +27,18 @@ var db = "Figure this out later";//mongoose.connect(connectionString);
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(session({
+    secret: 'this is the secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 4000;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 5000;
 app.get('/hello', function(req, res){
     res.send('hello world');
 });
